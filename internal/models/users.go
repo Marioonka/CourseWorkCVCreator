90fccf28e8560aa35fb9855699ebd5f7575c1b5a
Marina <marina02040667@gmail.com>
2024-11-24 22:30:35 +0300

    Delete all

--- internal/models/users.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type RegisterUsers struct {
-	ID       uint   `gorm:"primaryKey"`
-	Login    string `gorm:"unique"`
-	Password string
-	Resumes  []Resume `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-}
